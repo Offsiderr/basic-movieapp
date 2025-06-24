a2e6841b4fdaae2a267b581c1387283f9a6abaa4
BuildTools <unconfigured@null.spigotmc.org>
2025-06-24 13:54:02 +1200

    fixed tailwind for real

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: [
-    './app/**/*.{js, jsx, ts, tsx}',
-    './components/**/*.{js,jsx,ts,tsx}',
-    './app/**/(*?)/**/*.{js, jsx, ts, tsx}', // Covers folders with parentheses like (tabs), (auth)
-  ],
-  presets: [require('nativewind/preset')],
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colours:{
+        primary: '#030014',
+        secondary: '#151312',
+        light: {
+          100: '#D6C6FF',
+          200: '#A8B5DB',
+          300: '#9CA4AB',
+        },
+        dark: {
+          100: '#221f3d',
+          200: '#0f0d23',
+        },
+        accent: '#AB8BFF'
+      }
+    },
   },
   plugins: [],
-};
+}
